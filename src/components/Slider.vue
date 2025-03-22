5b0012464616a8b75ac4b2c2e46106aa974979c9
LucaFabbozzo <lucafabbozzo@gmail.com>
2025-03-22 16:21:06 +0100

    Add site title styling and conditionally render navigation buttons in slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -33,31 +33,13 @@
                 </div>
             </div>
         </div>
-        <button class="prev" @click="prevSlide"><i class="fa-solid fa-chevron-left"></i></button>
-        <button class="next" @click="nextSlide"><i class="fa-solid fa-chevron-right"></i></button>
+        <div v-if="route.name && route.name.startsWith('work')">
+            <button class="prev" @click="prevSlide"><i class="fa-solid fa-chevron-left"></i></button>
+            <button class="next" @click="nextSlide"><i class="fa-solid fa-chevron-right"></i></button>
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
 @use "@/assets/slider.scss";
-
-.site-title {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 500;
-    font-size: 3rem;
-    font-weight: bold;
-    color: white;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
-    pointer-events: none; // Evita che interferisca con i clic
-
-    /* Cornice rettangolare trasparente */
-    padding: 20px 40px; // Spaziatura interna
-    border: 2px solid rgba(255, 255, 255, 0.7); // Bordo bianco trasparente
-    background-color: rgba(0, 0, 0, 0.5); // Sfondo nero trasparente
-    border-radius: 10px; // Angoli arrotondati
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3); // Ombra per un effetto più elegante
-}
 </style>
