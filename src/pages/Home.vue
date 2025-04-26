a659183da148894afc193f060122e8840e18363a
LucaFabbozzo <lucafabbozzo@gmail.com>
2025-04-26 19:05:45 +0200

    Add logo image and update Home.vue; fix image reference for Work 6 in Works.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
 import { Cloudinary } from '@cloudinary/url-gen';
 import Slider from '@/components/Slider.vue';
 import Footer from '@/components/Footer.vue';
+import logo from '@/assets/logo.png';
 
 
 const cld = new Cloudinary({
@@ -11,11 +12,13 @@ const cld = new Cloudinary({
 });
 
 const img1 = cld.image('home_slide-1098_qhsot5');
-const img2 = cld.image('home_slide-7110_uctvgo');
-const img3 = cld.image('home_slide-6353_spe7kq');
-const img4 = cld.image('works_slide-2104_hsw2hh');
-const img5 = cld.image('home_slide-5_igght7');
-const img6 = cld.image('home_slide-3930_xj5if3');
+const img2 = cld.image('home_slide-3930_xj5if3');
+const img3 = cld.image('home_slide-3648_lasdlh');
+const img4 = cld.image('home_slide-3705_ofwh7h');
+const img5 = cld.image('home_slide-2_ns3fqp');
+const img6 = cld.image('home_slide-3621_sykq0w');
+const img7 = cld.image('home_slide-5577_rebvtl');
+const img8 = cld.image('home_slide-1_rhdhf9');
 
 // Creo un'array di oggetti per la slide
 const slides = [
@@ -24,13 +27,30 @@ const slides = [
   { class: 'img_3', src: img3 },
   { class: 'img_4', src: img4 },
   { class: 'img_5', src: img5 },
-  { class: 'img_6', src: img6 }
+  { class: 'img_6', src: img6 },
+  { class: 'img_7', src: img7 },
+  { class: 'img_8', src: img8 }
 ];
 </script>
 
 
 <template>
+  <div class="logo-container">
+        <img :src="logo" alt="Logo" class="logo" />
+    </div>
     <Slider :slides="slides"/>
     <Footer />
 </template>
 
+<style scoped>
+.logo-container {
+    position: absolute;
+    top: 5px;
+    z-index: 1000;
+}
+
+.logo {
+    width: 280px; /* Regola la dimensione del logo */
+    height: auto;
+}
+</style>
